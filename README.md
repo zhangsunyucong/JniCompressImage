701e7637ec6df253fa7d4a4a2eb603e5e196abfa
zhangsunyucong <969887864@qq.com>
2018-04-18 14:51:41 +0800

    ss

--- README.md
+++ README.md
@@ -1,1 +1,50 @@
 # JniCompressImage
+
+1、AndroidManifest.xml中授权限。因为要从手机读取图片，压缩后写回手机本地。
+
+```
+    <uses-permission android:name="android.permission.READ_EXTERNAL_STORAGE" />
+    <uses-permission android:name="android.permission.WRITE_EXTERNAL_STORAGE" />
+    <uses-permission android:name="android.permission.MOUNT_UNMOUNT_FILESYSTEMS" />
+    <uses-permission android:name="android.permission.WRITE_EXTERNAL_STORAGE" />
+```
+
+2、将main\cpp下的文件复制到自己项目的main\cpp目录下。
+
+3、将app\libs下的文件复制到自己项目的app\libs目录下。
+
+4、将app\CMakeLists.txt复制到自己项目的app\CMakeLists.txt下
+
+5、修改main\cpp\effective-bitmap.c文件的compressBitmap方法的路径。
+
+6、修改app\build.gradle,
+
+```
+//defaultConfig节点中添加
+
+externalNativeBuild {
+    cmake {
+        cppFlags ""
+    }
+}
+
+ndk {
+   abiFilters 'armeabi'
+}
+
+//在android节点中添加
+sourceSets {
+    main {
+        jni.srcDirs = []
+        jniLibs.srcDirs = ['libs']
+    }
+}
+
+externalNativeBuild {
+    cmake {
+        path "CMakeLists.txt"
+    }
+}
+```
+
+
---
+++ app/src/main/java/com/ctrip/zhshan/myapplication/EffectiveBitmapUtils.java
@@ -0,0 +1,94 @@
+package com.ctrip.zhshan.myapplication;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Rect;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+
+/**
+ * Created by Ricky on 2018/4/12.
+ */
+
+public class EffectiveBitmapUtils {
+
+    static {
+        System.loadLibrary("jpegbither");
+        System.loadLibrary("effective-bitmap");
+    }
+
+    public static native String compressBitmap(Bitmap bit, int w, int h, int quality, byte[] fileNameBytes,
+                                               boolean optimize);
+
+    private static int DEFAULT_QUALITY = 30;
+
+    public static void compressByJNI(Bitmap bit, String fileName, boolean optimize) {
+        saveBitmap(bit, DEFAULT_QUALITY, fileName, optimize);
+    }
+
+    private static void saveBitmap(Bitmap bit, int quality, String fileName, boolean optimize) {
+        compressBitmap(bit, bit.getWidth(), bit.getHeight(), quality, fileName.getBytes(), optimize);
+    }
+
+    public static void compressByQuality(Bitmap bmp, File file) {
+        int options = DEFAULT_QUALITY;
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        bmp.compress(Bitmap.CompressFormat.JPEG, options, baos);
+        try {
+            FileOutputStream fos = new FileOutputStream(file);
+            fos.write(baos.toByteArray());
+            fos.flush();
+            fos.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void compressBySize(Bitmap bmp, File file) {
+        int ratio = 8;
+        Bitmap result = Bitmap.createBitmap(bmp.getWidth() / ratio, bmp.getHeight() / ratio, Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(result);
+        Rect rect = new Rect(0, 0, bmp.getWidth() / ratio, bmp.getHeight() / ratio);
+        canvas.drawBitmap(bmp, null, rect, null);
+
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        result.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+        try {
+            FileOutputStream fos = new FileOutputStream(file);
+            fos.write(baos.toByteArray());
+            fos.flush();
+            fos.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void compressBySample(String filePath, File file) {
+        int inSampleSize = 8;
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = false;
+        options.inSampleSize = inSampleSize;
+        Bitmap bitmap = BitmapFactory.decodeFile(filePath, options);
+
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+        try {
+            if (file.exists()) {
+                file.delete();
+            } else {
+                file.createNewFile();
+            }
+            FileOutputStream fos = new FileOutputStream(file);
+            fos.write(baos.toByteArray());
+            fos.flush();
+            fos.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+}
